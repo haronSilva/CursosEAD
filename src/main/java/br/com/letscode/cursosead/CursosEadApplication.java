a093eea450acf81d99d65aba3af79de6ce946403
haronSilva <haron.silva@gmail.com>
2022-04-27 21:10:48 +0200

    Aula 27/04/2022

--- src/main/java/br/com/letscode/cursosead/CursosEadApplication.java
+++ src/main/java/br/com/letscode/cursosead/CursosEadApplication.java
@@ -5,13 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @Configuration
 @Slf4j
 @EnableCaching
--- src/main/java/br/com/letscode/cursosead/security/SegurancaConfig.java
+++ src/main/java/br/com/letscode/cursosead/security/SegurancaConfig.java
@@ -1,32 +1,51 @@
 package br.com.letscode.cursosead.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("haron")
-                .password("admin")
+
+    private UserDetailsService userDetailsServiceCustom() {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails admin = User.withUsername("haron")
+                .password(passwordEncoder.encode("admin"))
                 .roles("ADMIN")
                 .build();
+        UserDetails aluno = User.withUsername("aluno")
+                .password(passwordEncoder.encode("aluno"))
+                .roles("USER")
+                .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(admin, aluno);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceCustom());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and().httpBasic();
+                .antMatchers("/alunos").hasAnyRole("ADMIN","USER")
+                .antMatchers("/alunos/**").hasRole("ADMIN")
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .usernameParameter("usu").passwordParameter("psw").defaultSuccessUrl("/alunos")
+                        .and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/403");
+
+        http.csrf().disable();
     }
 }
